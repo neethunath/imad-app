c357a1313addac652585776c472f441f18c1686c
Neethu G Nath <neethugnath49@gmail.com>
2017-08-14 14:46:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function (req, res) {
   //articleName = article-one
   //articles[articleName] = {} content object for article one
-  var articleName = req.params.articlName;
+  var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
