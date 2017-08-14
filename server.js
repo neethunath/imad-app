3a3dfb7967fe08299fe22796a5448bf56d62ec7a
Neethu G Nath <neethugnath49@gmail.com>
2017-08-14 08:42:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,45 +24,53 @@ var articleone = {
            </p>`
 };
 
-var htmlTemplate= `
-   <!DOCTYPE html>
-  <head>
-     <title> 
-       ${title}
-     </title> 
-     <link href="/ui/style.css" rel="stylesheet" />
-     <meta name="viewport" content="width=device-width" initial-scale=1 />
-  </head>  
-  <body>
+function createTemplate (data) {
+  var title = data.title;
+  var date = data.date;
+  var heading = data.heading;
+  var content = data.content;
+  
+  var htmlTemplate = `
+    <!DOCTYPE html>
+      <head>
+      <title> 
+        ${title}
+      </title> 
+      <link href="/ui/style.css" rel="stylesheet" />
+      <meta name="viewport" content="width=device-width" initial-scale=1 />
+      </head>  
+      <body>
      
-     <div class="container">
-      <div>
-          <a href="/">Home</a>
-      </div>
+        <div class="container">
+           <div>
+              <a href="/">Home</a>
+           </div>
       
-      <hr/>
+           <hr/>
       
-      <h3>
-          ${heading}
-      </h3>
+           <h3>
+              ${heading}
+           </h3>
       
-      <div>
-          ${date}
-      </div>
+           <div>
+              ${date}
+           </div>
       
-      <div>
-          ${content}
-      </div>
-      </div>
-  </body>  
+           <div>
+              ${content}
+           </div>
+       </div>
+     </body>  
 </html>
 `;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleone));
 });
 
 app.get('/article-two', function (req, res) {
